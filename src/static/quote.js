c8af3140c0faea45be929acb90b48c908db89f9a
Ian Goodrich <goodrichian@gmail.com>
2025-08-27 13:36:30 +0400

    fix: scroll to top on hard refresh

--- src/static/quote.js
+++ src/static/quote.js
@@ -2,6 +2,11 @@ document.addEventListener("DOMContentLoaded", () => {
     const refreshButton = document.querySelector(".refresh-btn");
     const quoteText = document.querySelector(".quote-text");
 
+    if (quoteText) {
+        // Reset the scroll position to the top on page load
+        quoteText.scrollTop = 0;
+    }
+
     if (refreshButton && quoteText) {
         refreshButton.addEventListener("click", () => {
             // Reset the scroll position of the quote text container
